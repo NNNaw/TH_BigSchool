e0e55ece79891a47d6946e2e90be771bcc431a7d
NNNaw <nhatnam.nx3@gmail.com>
2020-06-07 22:38:58 +0700

    Add project files.

---
+++ TH_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TH_BigSchool.MvcApplication" Language="C#" %>
---
+++ TH_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TH_BigSchool.Startup))]
+namespace TH_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
